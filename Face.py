deb6d01ecc614c9a42dc1001d272b53f52b17224
Kaarlo <kaarlocaballeronillukka@gmail.com>
2024-08-08 13:18:57 +0100

    Fully functional project commented and organised

--- Face.py
+++ Face.py
@@ -3,27 +3,27 @@ import cv2
 import numpy as np
 from deepface import DeepFace
 
+face_detector = cv2.CascadeClassifier(
+    'haarcascade_frontalface_default.xml')  # initialize the haar cascade face detector
 
-face_detector = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')  # initialize the classifier
 
-
-class Face:
-    detected_faces = []
-    face_ID = 0
+class Face:  # create a class to manage all faces detected
+    detected_faces = []  # a list that will contain all faces detected in the current frame
+    face_ID = 0  # an ID that will identify every face
 
     def __init__(self, center: tuple, frame_counter, gender: str, color=None):
-        Face.face_ID += 1
-        self.face_ID = Face.face_ID
-        self.center = center
-        self.last_frame = frame_counter
-        self.gender = gender
-        self.color = color
+        Face.face_ID += 1   # add one to the faceID, so each new face has a different ID
+        self.face_ID = Face.face_ID # faceID for the new detected face
+        self.center = center    # center of the rectangle of the new detected face
+        self.last_frame = frame_counter # last frame in which the detected face has appeared
+        self.gender = gender    # gender of the new detected face
+        self.color = color  # color of the rectangle around the detected face
 
-    def __str__(self):
+    def __str__(self):  # informal representation of a face
         return f'[{self.face_ID}, {self.center}, {self.gender}, {self.color}]'
 
 
-def delete_face(frame_counter):
+def delete_face(frame_counter):  # if a face in detected faces is not detected after 30 frames(around a second), it is deleted from the detected faces
     for face in Face.detected_faces:
         if frame_counter - face.last_frame > 30:
             Face.detected_faces.remove(face)
@@ -31,68 +31,66 @@ def delete_face(frame_counter):
 
 
 def predict_gender(frame, x, y, width, height, model):
-    frame = frame[y:y + height, x:x + width]
-    frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
-    frame = np.array(frame)
+    frame = frame[y:y + height, x:x + width]    # crop the frame, so it only has they face already detected
+    frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)  # change the image from RGB format to BGR format so DeepFace can process it
+    frame = np.array(frame)  # change the image to numpy array format so DeepFace can process it
     embedding_objs = DeepFace.represent(img_path=frame,
                                         model_name='Facenet512',
                                         detector_backend='opencv',
                                         enforce_detection=False
-                                        )
-    embedding = embedding_objs[0]["embedding"]
-    gender = model.predict(np.array(embedding).reshape(1, -1))[0]
-    return gender
+                                        )   # use DeepFace to change the image from numpy array to a characteristics vector that can be processed by our model
+    embedding = embedding_objs[0]["embedding"]  # get the characteristics vector
+    return model.predict(np.array(embedding).reshape(1, -1))[0]   # use our model to predict the gender of the face detected
 
 
 def detect_faces(frame, frame_counter, model):
-    faces = face_detector.detectMultiScale(frame)  # detect faces in frame
-    for (x, y, width, height) in faces:
-        if 150 <= width <= 370 or 150 <= height <= 370:  # threshold of face size
-            center = (int(x + width / 2), int(y + height / 2))  # Center of the face
-            face = identify_faces(center, frame_counter, frame, x, y, width, height, model)
-            if face is None:
-                face = new_face(center, frame_counter, frame, x, y, width, height, model)
-            #gender = predict_gender(frame, x, y, width, height)
-            delete_face(frame_counter)  # delete faces that are not in the camara for 30 frames
+    faces = face_detector.detectMultiScale(frame)  # detect faces in the frame
+    for (x, y, width, height) in faces: # for every face get, x, y, width and height of teh rectangle around it
+        if 150 <= width <= 370 or 150 <= height <= 370:  # threshold of detected face rectangle size
+            center = (int(x + width / 2), int(y + height / 2))  # Center of the rectangle
+            face = identify_faces(center, frame_counter, frame, x, y, width, height, model)  # verify if this face has already been detected
+            if face is None:    # if this face is new
+                face = new_face(center, frame_counter, frame, x, y, width, height, model)   # create a new face
+            delete_face(frame_counter)  # delete faces that are not in the frame for 30 frames(around a second)
             cv2.rectangle(frame, (x, y), (x + width, y + height), face.color, 2)  # draw rectangle in frame
             cv2.putText(frame, f"{face.face_ID}-{face.gender}", (x, y - 10),
-                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, face.color, 2)  # draw face_ID in frame
-            #cv2.putText(frame, f"{face.gender}", (x, y - 10),
-            #cv2.FONT_HERSHEY_SIMPLEX, 0.5, face.color, 2)  # draw face_ID in frame
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, face.color, 2)  # draw face_ID and gender on the rectangle
         else:
             continue
-    return frame
+    return frame  # return de frame fully processed
 
 
 def min_distance_face(center, frame_counter):
-    face_distances = []
-    for idx, face in enumerate(Face.detected_faces):
-        face_distances.append([idx, abs(face.center[0] - center[0]) + abs(face.center[1] - center[1])])
-    closest_face = min(face_distances, key=lambda element: element[1])
-    if closest_face[1] >= 60:
+    face_distances = [] # list that will save the index fo the face in the list and the distance to the current detected face
+    for idx, face in enumerate(Face.detected_faces):    # for every face in detected faces, get index in the list and face
+        face_distances.append([idx, abs(face.center[0] - center[0]) + abs(face.center[1] - center[1])])  # add to faces_distances a list that has
+                                                                                                         # the index of the face in detected faces
+                                                                                                         # and the manhattan distances
+    closest_face = min(face_distances, key=lambda element: element[1])  # get the closest face to the current detected face
+    if closest_face[1] >= 60:   # if the distance is more than 60
         return None
     else:
-        update_face(Face.detected_faces[closest_face[0]], center, frame_counter)
-        return Face.detected_faces[closest_face[0]]
+        update_face(Face.detected_faces[closest_face[0]], center, frame_counter)    # update the values needed in the current detected face
+        return Face.detected_faces[closest_face[0]] # return the face current detected face
 
 
 def new_face(center, frame_counter, frame, x, y, width, height, model):
-    color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
-    gender = predict_gender(frame, x, y, width, height, model)
-    face = Face(center, frame_counter, gender, color)
-    print(f"new face: {face}")
-    Face.detected_faces.append(face)
+    color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))    # select a random color for the rectangle
+    gender = predict_gender(frame, x, y, width, height, model)  # predict the gender of the face with predict_gender()
+    face = Face(center, frame_counter, gender, color)   # create a new face
+    print(f"new face: {face}")  # print new face to know what is happening
+    Face.detected_faces.append(face)    # add new face to detected faces
     return face
 
 
 def update_face(face, center, frame_counter):
-    face.center = center
-    face.last_frame = frame_counter
+    face.center = center    # update the center value of the current detected face using the new center
+    face.last_frame = frame_counter # update the last frame of the current detected face using the current frame
     return face
 
 
 def identify_faces(center, frame_counter, frame, x, y, width, height, model):
-    if not Face.detected_faces:
+    if not Face.detected_faces:  # if detected faces is empty, create and return a new face
         return new_face(center, frame_counter, frame, x, y, width, height, model)
     else:
-        return min_distance_face(center, frame_counter)
+        return min_distance_face(center, frame_counter)  # check the closest face
--- Main.py
+++ Main.py
@@ -1,23 +1,41 @@
 import cv2
 from Face import detect_faces
-from GenderModelPreparation import train_gender_model
+import pandas as pd
+from sklearn.svm import SVC
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split
 
-camara = cv2.VideoCapture(0)
+
+def train_gender_model():
+    data = pd.read_csv('data\\Images_data.csv') # create dataframe from the data gathered
+    y = data.iloc[:, 1].values  # establish y values, which will be the results to predict
+    x = data.iloc[:, 2:].values # establish x values, which will be the entering values from which the model will predict y
+    train_x, test_x, train_y, test_y = train_test_split(x, y, test_size=0.1, random_state=0, shuffle=True)  # divide data in train and test
+
+    clf = SVC(C=3, kernel='rbf')    # create the instance of the model with the hyperparameters that maximize its performance
+    clf.fit(train_x, train_y)   # train the model
+    y_pred = clf.predict(test_x)    # use the x values from the test data set to evaluate its performance after training
+    accuracy = round(accuracy_score(test_y, y_pred) * 100, 2)   # calculate the accuracy, based on how many predictions were correct
+    print(f"Accuracy: {accuracy}%\n")   # print the accuracy result
+    return clf  # return de trained model
+
+
+camara = cv2.VideoCapture(0)    # start the camera of the computer
 if not camara.isOpened():  # checks if camara is working
-    print("Camara not working.")
-    exit()
-frame_counter = 0
-model = train_gender_model()
+    print("Camara not working.")    # print a message to know what is happening
+    exit()  # close camera
+frame_counter = 0   # variable to know in what frame are we currently, so we can know when a person is not on the frame
+model = train_gender_model()    # we train and get de model ready to use
 while True:
-    ret, frame = camara.read()  # returns the frame
+    ret, frame = camara.read()  # returns the camera frame and a boolean indicating if its working
     if not ret:  # if there's no frame
-        print("No frame received.")
-        break
+        print("No frame received.") # print a message to know what is happening
+        break   # exit the loop
 
-    cv2.imshow('camara', detect_faces(frame, frame_counter, model))
+    cv2.imshow('camara', detect_faces(frame, frame_counter, model)) # show image once it's processed by detect_faces()
     frame_counter += 1
 
-    if cv2.waitKey(1) == ord('q'):
+    if cv2.waitKey(1) == ord('q'):  # if 'q' button is pressed, close the camera, and close all windows opened by cv2
         camara.release()
         cv2.destroyAllWindows()
         break
